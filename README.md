29c821e04c02c4c459b0f434ffe53fbdd2e8059f
laluxx <lorenzoaddamo10@gmail.com>
2023-11-30 08:13:37 +0100

    updato

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # TODO
 
 ## Evil mode
-- [ ] Built in EVIL mode
+- [ ] it takes 2 keystrokes to go in NORMAL mode
 - [ ] keychords in normal mode
-- [ ] cursor color change based on mode (and stay a block)
+- [x] cursor color change based on mode (and stay a block)
 
 ## General
-- [ ] Ctrl-z should not kill the shell
+- [ ] Ctrl-backspace delete all to the left 
 - [ ] Dont depend on pywal, simply grab the first 16 colors somehow
 - [ ] Kill existing rsh process when spawning a new one from rsh or probably we should kill any other shell idk the standard
 - [ ] `cd rsh && ls` DONT work for some reason
@@ -16,6 +16,7 @@
 - [ ] make loops possible by writing a new line instead of immediate execution (like xonsh)
 - [ ] Cool stuff like `p = pwd`
 - [ ] aliases and functions should be in `~/.config/rsh/config.rb`
+- [x] Ctrl-z should not kill the shell
 
 ## Functions
 - [ ] while this file doesnt exist do
@@ -30,3 +31,53 @@
 - [x] Aliases
 - [x] Functions
 - [x] Keybinds like ctrl+l up arrow down arrow
+- [x] Prompt color based on success or fail
+
+
+
+# Old version
+too high level
+```ruby
+require 'readline'
+# History
+HISTORY_FILE = File.expand_path('~/.config/rsh/history')
+FileUtils.mkdir_p(File.dirname(HISTORY_FILE))
+Readline::HISTORY.push(*File.readlines(HISTORY_FILE).map(&:chomp))
+
+# Tab Completion
+Readline.completion_proc = proc do |s|
+  Dir["#{s}*"].grep(/^#{Regexp.escape(s)}/)
+end
+
+last_command_success = nil
+# Main loop
+begin
+  puts "Welcome to the crystal shell - Type 'exit' to quit."
+
+  Signal.trap("SIGINT") do
+    last_command_success = true
+    Readline.point = 0
+    Readline.delete_text
+    Readline.redisplay
+    puts "\n"
+    print "#{RESET_COLOR}❯ "
+  end
+
+  while input = Readline.readline("#{last_command_success.nil? ? '' : (last_command_success ? GREEN : RED)}❯ #{RESET_COLOR}", false)
+    break if input.nil? || input == "exit"
+
+    # Remove duplicates and add non-empty commands to history manually
+    unless input.strip.empty?
+      # Run the command and update the success status
+      last_command_success = run_command(input, aliases, functions)
+      Readline::HISTORY.pop if Readline::HISTORY.length > 0 && Readline::HISTORY[-1] == input
+      Readline::HISTORY.push(input)
+    end
+  end
+ensure
+  # Save the history when exiting
+  File.open(HISTORY_FILE, 'w') do |file|
+    Readline::HISTORY.to_a.last(100).each { |cmd| file.puts(cmd) }
+  end
+end
+```
--- rsh.rb
+++ rsh.rb
@@ -2,7 +2,7 @@
 
 require 'io/console'
 require 'fileutils'
-require 'readline'
+
 
 aliases = {
   "p" => "puts Dir.pwd",
@@ -14,6 +14,17 @@ aliases = {
   "get" => "sudo pacman -S",
   # more aliases...
 }
+
+
+def read_pywal_colors
+  wal_colors_file = File.expand_path('~/.cache/wal/colors')
+  if File.exist?(wal_colors_file)
+    colors = File.readlines(wal_colors_file).map(&:chomp)
+    { background: colors[0], text: colors[7] }
+  else
+    { background: '#000000', text: '#FFFFFF' } # Default colors if file not found TODO
+  end
+end
 
 functions = {
   "sum" => ->(args) { args.map(&:to_i).sum },
@@ -49,16 +60,19 @@ functions = {
   # More functions...
 }
 
-def read_pywal_colors
-  wal_colors_file = File.expand_path('~/.cache/wal/colors')
-  if File.exist?(wal_colors_file)
-    colors = File.readlines(wal_colors_file).map(&:chomp)
-    { background: colors[0], text: colors[7] }
-  else
-    { background: '#000000', text: '#FFFFFF' } # Default colors if file not found TODO
-  end
-end
+TRUE_GREEN = "\e[38;2;0;255;0m"
+TRUE_RED = "\e[38;2;255;0;0m"
 
+
+BLACK   = "\e[30m"
+RED     = "\e[31m"
+GREEN   = "\e[32m"
+YELLOW  = "\e[33m"
+BLUE    = "\e[34m"
+MAGENTA = "\e[35m"
+CYAN    = "\e[36m"
+WHITE   = "\e[37m"
+RESET_COLOR = "\e[0m"
 
 def substitute_aliases(command, aliases)
   command.split.map { |word| aliases.fetch(word, word) }.join(" ")
@@ -76,68 +90,200 @@ def run_command(command, aliases, functions)
 
   if functions.key?(command_name)
     puts functions[command_name].call(args)
+    true
   elsif command.strip.start_with?("cd ")
     dir = args[0] || ENV['HOME']
     begin
       Dir.chdir(dir)
+      true
     rescue SystemCallError => e
       puts "Error changing directory: #{e}"
+      false
     end
   elsif valid_system_command?(command)
     begin
       system(command)
     rescue Interrupt
       puts "\nCommand interrupted"
+      false
+    else
+      true
     end
   else
     begin
       eval(command)
+      true
     rescue SyntaxError, NameError => e
       puts "rsh: command not found: #{command}"
+      false
     rescue Interrupt
       puts "\nRuby evaluation interrupted"
+      false
     end
   end
 end
 
-# History file handling
+
+# Constants and Setup
 HISTORY_FILE = File.expand_path('~/.config/rsh/history')
 FileUtils.mkdir_p(File.dirname(HISTORY_FILE))
-Readline::HISTORY.push(*File.readlines(HISTORY_FILE).map(&:chomp))
+history = File.readlines(HISTORY_FILE).map(&:chomp)
+history_index = history.length
 
-# Tab Completion (optional)
-Readline.completion_proc = proc do |s|
-  Dir["#{s}*"].grep(/^#{Regexp.escape(s)}/)
+INSERT_MODE = :insert
+NORMAL_MODE = :normal
+current_mode = INSERT_MODE
+
+# Custom input handling
+def custom_readline(prompt, history, history_index)
+  print prompt
+  input = ''
+  cursor_position = 0
+
+  while char = STDIN.raw(&:getch)
+    case char
+    when "\r" # Enter key
+      puts
+      return input, history_index
+    when "\u007F", "\b" # Backspace key
+      if cursor_position > 0
+        input.slice!(-1)
+        cursor_position -= 1
+        print "\b \b"
+      end
+    when "\e" # Escape sequence
+      next_char = STDIN.getch
+      if next_char == '['
+        arrow_key = STDIN.getch
+        case arrow_key
+        when 'A' # Up arrow
+          if history_index > 0
+            history_index -= 1
+            input = history[history_index]
+            print "\r#{prompt}#{input}#{" " * [0, cursor_position - input.length].max}"
+            cursor_position = input.length
+          end
+        when 'B' # Down arrow
+          if history_index < history.length - 1
+            history_index += 1
+            input = history[history_index]
+            print "\r#{prompt}#{input}#{" " * [0, cursor_position - input.length].max}"
+            cursor_position = input.length
+          end
+        end
+      else
+        return :switch_mode, input, history_index
+      end
+    when "\u0003" # Ctrl+C
+      exit 0
+    when "\u000C" # Ctrl+L
+      system("clear")
+      print prompt + input
+    else
+      input << char
+      cursor_position += 1
+      print char
+    end
+  end
 end
+
+# Handling input in normal mode
+def handle_normal_mode_input
+  while char = STDIN.raw(&:getch)
+    case char
+    when "i"
+      return :switch_mode
+    when "h"  # Left
+      print "\e[D"
+    when "j"  # Down
+      print "\e[B"
+    when "k"  # Up
+      print "\e[A"
+    when "l"  # Right
+      print "\e[C"
+    end
+  end
+end
+
+
+
+
+
+def extract_pywal_colors
+  wal_colors_file = File.expand_path('~/.cache/wal/colors')
+  colors = {}
+
+  if File.exist?(wal_colors_file)
+    wal_colors = File.readlines(wal_colors_file).map(&:chomp)
+    colors.merge!(
+      BLACK: wal_colors[0],
+      RED: wal_colors[1],
+      GREEN: wal_colors[2],
+      YELLOW: wal_colors[3],
+      BLUE: wal_colors[4],
+      MAGENTA: wal_colors[5],
+      CYAN: wal_colors[6],
+      WHITE: wal_colors[7]
+    )
+  end
+  colors
+end
+
+pywal = extract_pywal_colors
+
+BLACK_WAL = pywal[:BLACK]
+RED_WAL = pywal[:RED]
+GREEN_WAL = pywal[:GREEN]
+YELLOW_WAL = pywal[:YELLOW]
+BLUE_WAL = pywal[:BLUE]
+MAGENTA_WAL = pywal[:MAGENTA]
+CYAN_WAL = pywal[:CYAN]
+WHITE_WAL = pywal[:WHITE]
+
+
+
+NORMAL_MODE_CURSOR_COLOR = BLUE_WAL
+INSERT_MODE_CURSOR_COLOR = WHITE_WAL
+# NORMAL_MODE_CURSOR_COLOR = "\e]12;red\a"   # It set the bg color ?
+# INSERT_MODE_CURSOR_COLOR = "\e]12;green\a" # It set the bg color ?
+
 
 # Main loop
 begin
   puts "Welcome to the crystal shell - Type 'exit' to quit."
+  last_command_success = true
 
-  Signal.trap("SIGINT") do
-    # Clear the current input buffer
-    Readline.point = 0
-    Readline.delete_text
-    Readline.redisplay
+  loop do
+    if current_mode == INSERT_MODE
+      # print INSERT_MODE_CURSOR_COLOR
+      # print "\e]10;#{INSERT_MODE_CURSOR_COLOR}\a"  # Set text color
+      print "\e]12;#{INSERT_MODE_CURSOR_COLOR}\a"  # Set cursor color
 
-    # Move to a new line and show the prompt
-    puts "\n"
-    print "❯ "
-  end
 
-  while input = Readline.readline("❯ ", false)  # Set 'false' to not add to history automatically
-    break if input.nil? || input == "exit"
+      prompt = "#{last_command_success ? GREEN : RED}❯ #{RESET_COLOR}"
+      input, history_index = custom_readline(prompt, history, history_index)
+      if input == :switch_mode
+        current_mode = NORMAL_MODE
+        next
+      end
+      break if input == "exit"
 
-    # Remove duplicates and add non-empty commands to history manually
-    unless input.strip.empty?
-      run_command(input, aliases, functions)
-      Readline::HISTORY.pop if Readline::HISTORY.length > 0 && Readline::HISTORY[-1] == input
-      Readline::HISTORY.push(input)
+      unless input.strip.empty?
+        last_command_success = run_command(input, aliases, functions)
+        history.push(input) unless history.last == input
+        history_index = history.length
+      end
+    else # NORMAL_MODE
+      # print NORMAL_MODE_CURSOR_COLOR
+      # print "\e]10;#{NORMAL_MODE_CURSOR_COLOR}\a"  # Set text color
+      print "\e]12;#{NORMAL_MODE_CURSOR_COLOR}\a"  # Set cursor color
+
+
+      action = handle_normal_mode_input
+      current_mode = INSERT_MODE if action == :switch_mode
     end
   end
 ensure
-  # Save the history when exiting
-  File.open(HISTORY_FILE, 'w') do |file|
-    Readline::HISTORY.to_a.last(100).each { |cmd| file.puts(cmd) }
-  end
+  # Save history when exiting
+  File.open(HISTORY_FILE, 'w') { |file| history.each { |cmd| file.puts(cmd) } }
 end
